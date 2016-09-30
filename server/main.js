7c53042e73f10cc5fe20c529828ff9bed749152b
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-10-01 00:24:18 +0100

    Create npm start commands for different environments

--- server/main.js
+++ server/main.js
@@ -38,10 +38,7 @@ if (config.env === 'development') {
 } else {
   debug(
     'Server is being run outside of live development mode, meaning it will ' +
-    'only serve the compiled application bundle in ~/dist. Generally you ' +
-    'do not need an application server for this and can instead use a web ' +
-    'server such as nginx to serve your static files. See the "deployment" ' +
-    'section in the README for more information on deployment strategies.'
+    'only serve the compiled application bundle in ~/dist.'
   )
 
   // Serving ~/dist by default. Ideally these files should be served by
