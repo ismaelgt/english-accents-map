b4a4aaf0412bea56e610f50d64ea1ef2f55b4eb4
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-10-15 10:13:01 +0100

    Remove unneeded coverage lines from tests bundler

--- tests/test-bundler.js
+++ tests/test-bundler.js
@@ -29,9 +29,3 @@ const testsContext = require.context('./', true, /\.spec\.js$/)
 // only run tests that have changed after the first pass.
 const testsToRun = testsContext.keys().filter(inManifest)
 ;(testsToRun.length ? testsToRun : testsContext.keys()).forEach(testsContext)
-
-// require all `src/**/*.js` except for `main.js` (for isparta coverage reporting)
-if (__COVERAGE__) {
-  const componentsContext = require.context('../src/', true, /^((?!main|reducers).)*\.js$/)
-  componentsContext.keys().forEach(componentsContext)
-}
