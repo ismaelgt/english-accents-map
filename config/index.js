62279e7aa6bf92c8626510dc1bacbe8d17b40711
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-11-09 22:56:02 +0000

    Use localhost as server_host

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@
 const path = require('path')
 const debug = require('debug')('app:config')
 const argv = require('yargs').argv
-const ip = require('ip')
 
 debug('Creating default configuration.')
 // ========================================================
@@ -23,7 +22,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_host : 'localhost', // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
