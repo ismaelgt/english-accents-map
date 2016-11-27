3074cbee0b774ab375b0708e1fb6f2aab34ef736
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-11-27 10:51:25 +0000

    First tabs attempt

--- src/scenes/home/components/HomePage/HomePage.js
+++ src/scenes/home/components/HomePage/HomePage.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import Map from '../Map'
 import VideosList from '../VideosList'
+import './styles.scss'
 
 const HomePage = React.createClass({
   render () {
     const { viewport, children, videoOpen } = this.props
+
+    const mobileView = (
+      <div className='eam-tabs mdl-tabs mdl-js-tabs mdl-js-ripple-effect'>
+        <div className='mdl-tabs__tab-bar'>
+          <a href='#list' className='mdl-tabs__tab'>
+            <i className='material-icons'>view_list</i>
+          </a>
+          <a href='#map' className='mdl-tabs__tab is-active'>
+            <i className='material-icons'>map</i>
+          </a>
+        </div>
+        <div className='mdl-tabs__panel' id='list'>
+          { children }
+        </div>
+        <div className='mdl-tabs__panel is-active' id='map'>
+          <Map />
+        </div>
+      </div>
+    )
+
+    const desktopView = (
+      <div>
+        <Map />
+        { children }
+      </div>
+    )
+
     return (
       <div>
-        { viewport.isSmall ? null : <Map /> }
+        { viewport.isSmall ? mobileView : desktopView }
         { videoOpen ? <VideosList /> : null }
-        { children }
       </div>
     )
   },
