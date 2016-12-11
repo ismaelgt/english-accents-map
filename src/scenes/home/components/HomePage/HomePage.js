8278af853bac31350f586108e7ae04132ecf3113
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-12-11 08:56:41 +0000

    Reinitalise tabs on screen resize

--- src/scenes/home/components/HomePage/HomePage.js
+++ src/scenes/home/components/HomePage/HomePage.js
@@ -4,26 +4,32 @@ import VideosList from '../VideosList'
 import './styles.scss'
 
 const HomePage = React.createClass({
+  componentDidUpdate (prevProps) {
+    if (prevProps.viewport.isSmall !== this.props.viewport.isSmall && this.props.viewport.isSmall) {
+      componentHandler.upgradeDom()
+      this.refs.tabs.MaterialTabs.init()
+    }
+  },
   render () {
     const { viewport, children, accentSelected } = this.props
 
     const mobileView = (
-      <div className='eam-tabs mdl-tabs mdl-js-tabs mdl-js-ripple-effect'>
+      <div ref='tabs' className='eam-tabs mdl-tabs mdl-js-tabs mdl-js-ripple-effect'>
         <div className='mdl-tabs__tab-bar'>
-          <a href='#list' className='mdl-tabs__tab'>
-            <i className='material-icons'>view_list</i>
-          </a>
           <a href='#map' className='mdl-tabs__tab is-active'>
             <i className='material-icons'>map</i>
           </a>
+          <a href='#list' className='mdl-tabs__tab'>
+            <i className='material-icons'>view_list</i>
+          </a>
+        </div>
+        <div className='mdl-tabs__panel is-active' id='map'>
+          <Map />
         </div>
         <div className='mdl-tabs__panel' id='list'>
           <div className='eam-card-wrapper'>
             { children }
           </div>
-        </div>
-        <div className='mdl-tabs__panel is-active' id='map'>
-          <Map />
         </div>
       </div>
     )
