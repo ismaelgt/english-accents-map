be105ede8031b572c565fb1d955799dda4121787
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-12-15 17:51:35 +0000

    Favorite button tooltip

--- src/scenes/home/components/VideosList/AddToFavoritesButton.js
+++ src/scenes/home/components/VideosList/AddToFavoritesButton.js
@@ -1,19 +1,35 @@
 import React from 'react'
 
 const AddToFavouritesButton = React.createClass({
+
+  componentDidMount () {
+    componentHandler.upgradeDom()
+  },
+
+  componentDidUpdate () {
+    componentHandler.upgradeDom()
+  },
+
   isFavorite (id) {
     return this.props.favorites.indexOf(id) > -1
   },
+
   render () {
     return (
-      <button className={'mdl-button mdl-js-button mdl-button--icon videos-list__button-favorite' +
-        (this.isFavorite(this.props.id) ? ' is-active' : '')}
-        onClick={(evt) => { evt.stopPropagation(); this.props.toggleFavorite(this.props.id) }}
-      >
-        <i className='material-icons'>favorite</i>
-      </button>
+      <div>
+        <button id='add-to-favorites'
+          className={'mdl-button mdl-js-button mdl-button--icon videos-list__button-favorite' +
+            (this.isFavorite(this.props.id) ? ' is-active' : '')}
+          onClick={(evt) => { evt.stopPropagation(); this.props.toggleFavorite(this.props.id) }}>
+          <i className='material-icons'>favorite</i>
+        </button>
+        <div className='mdl-tooltip' data-mdl-for='add-to-favorites'>
+          { this.isFavorite(this.props.id) ? 'Remove from my favorites' : 'Add to my favorites' }
+        </div>
+      </div>
     )
   },
+
   propTypes: {
     id: React.PropTypes.string,
     favorites: React.PropTypes.array,
