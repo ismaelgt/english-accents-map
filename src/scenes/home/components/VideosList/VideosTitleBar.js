e3bf380de2ff0357f71fd1f851f749c458c906e4
Ismael González Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-12-16 09:28:30 +0000

    Fix close video on mobile view

--- src/scenes/home/components/VideosList/VideosTitleBar.js
+++ src/scenes/home/components/VideosList/VideosTitleBar.js
@@ -1,33 +1,45 @@
 import React from 'react'
+import { browserHistory } from 'react-router'
 
-const VideosTitleBar = ({ accents, accentSelected, selectAccent }) => {
-  return (
-    <div className='eam-card-wrapper'>
-      <div className='eam-card mdl-card mdl-shadow--8dp'>
-        <div className='mdl-card__title'>
-          <img className='mdl-list__item-avatar'
-            src={'/images/flags/' + accents.byId[accentSelected].country + '.svg'}
-            alt={accents.byId[accentSelected].country} />
-          <h2 className='mdl-card__title-text'>
-            { accents.byId[accentSelected].name }
-          </h2>
-        </div>
-        <div className='mdl-card__menu'>
-          <button className='mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'
-            onClick={() => selectAccent(null)}>
-            <i className='material-icons'>arrow_back</i>
-          </button>
+const VideosTitleBar = React.createClass({
+
+  closeVideo (evt) {
+    const { accents, accentSelected } = this.props
+
+    evt.stopPropagation()
+    browserHistory.push('/' + accents.byId[accentSelected].country + '/')
+  },
+
+  render () {
+    const { accents, accentSelected } = this.props
+    return (
+      <div className='eam-card-wrapper'>
+        <div className='eam-card mdl-card mdl-shadow--8dp'>
+          <div className='mdl-card__title'>
+            <img className='mdl-list__item-avatar'
+              src={'/images/flags/' + accents.byId[accentSelected].country + '.svg'}
+              alt={accents.byId[accentSelected].country} />
+            <h2 className='mdl-card__title-text'>
+              { accents.byId[accentSelected].name }
+            </h2>
+          </div>
+          <div className='mdl-card__menu'>
+            <button className='mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'
+              onClick={this.closeVideo}>
+              <i className='material-icons'>arrow_back</i>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  },
 
-VideosTitleBar.propTypes = {
-  accents: React.PropTypes.object,
-  accentSelected: React.PropTypes.string,
-  countrySelected: React.PropTypes.string,
-  selectAccent: React.PropTypes.func
-}
+  propTypes: {
+    accents: React.PropTypes.object,
+    accentSelected: React.PropTypes.string,
+    countrySelected: React.PropTypes.string,
+    selectAccent: React.PropTypes.func
+  }
+})
 
 export default VideosTitleBar
