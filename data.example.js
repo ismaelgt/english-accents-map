a962ed52df5fede909805ed00f2b9944cc1dfbf1
Ismael Gonzalez Trujillo <ismael.gonzaleztrujillo@gmail.com>
2016-12-22 11:00:02 +0100

    Add config and database examples

---
+++ data.example.js
@@ -0,0 +1,54 @@
+{
+  "countries" : {
+    "gb" : {
+      "coords" : {
+        "ne" : {
+          "lat" : 56.423256,
+          "lng" : -1.39493
+        },
+        "sw" : {
+          "lat" : 50.663662,
+          "lng" : -4.910555
+        }
+      },
+      "name" : "United Kingdom",
+      "published" : true,
+      "zoom" : 9
+    },
+    "us" : {
+      "coords" : {
+        "ne" : {
+          "lat" : 44.644828,
+          "lng" : -66.895662
+        },
+        "sw" : {
+          "lat" : 26.51186,
+          "lng" : -123.635286
+        }
+      },
+      "name" : "United States",
+      "published" : true,
+      "zoom" : 6
+    }
+  },
+  "accents" : {
+    "manchester" : {
+      "coords" : {
+        "lat" : 53.481154,
+        "lng" : -2.243506
+      },
+      "country" : "gb",
+      "name" : "Manchester (Mancunian)",
+      "videos" : [ "GsrZk99s9LY", "eB2QbnyBe6s" ]
+    },
+    "texas" : {
+      "coords" : {
+        "lat" : 31.485542,
+        "lng" : -99.101441
+      },
+      "country" : "us",
+      "name" : "Texas",
+      "videos" : [ "s3AuqeI51mY", "IzyTLJWNkas" ]
+    }
+  }
+}
---
+++ src/config.example.js
@@ -0,0 +1,10 @@
+export const ANALYTICS_TRACKING_ID = 'UA-xxxxxxxx-x'
+export const GOOGLE_MAPS_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+export const FIREBASE_CONFIG = {
+  apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  authDomain: 'your-firebase-app.firebaseapp.com',
+  databaseURL: 'https://your-firebase-app.firebaseio.com',
+  storageBucket: 'your-firebase-app.appspot.com',
+  messagingSenderId: 'xxxxxxxxxxxx'
+}
